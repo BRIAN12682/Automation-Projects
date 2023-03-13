0780989f61cf4b1f070f0c7dd7def3646394f49c
ALABYEKKUBO SSUUBI BRIAN <106748613+BRIAN12682@users.noreply.github.com>
2023-03-13 19:41:11 +0300

    ubuntu testing

---
+++ firstgame.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((300,400))
+status = False
+
+while status:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status = True
+    pygame.display.flip()
