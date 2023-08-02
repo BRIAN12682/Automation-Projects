4ce641e59988bd2c5334a2f55ec52da43b463f2b
ALABYEKKUBO SSUUBI BRIAN <106748613+BRIAN12682@users.noreply.github.com>
2023-08-02 10:54:36 +0300

    Update tkinterwebview.py

--- tkinterwebview.py
+++ tkinterwebview.py
@@ -1,1 +1,12 @@
-
+from tkinter import *
+import webview
+  
+# define an instance of tkinter
+tk = Tk()
+  
+#  size of the window where we show our website
+tk.geometry("800x450")
+  
+# Open website
+webview.create_window('CHATGPT', 'https://chat.openai.com/')
+webview.start()
