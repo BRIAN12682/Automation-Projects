f1c13e8e8ef0f8a9043175321dd10bbf9c2f0eb7
ALABYEKKUBO SSUUBI BRIAN <106748613+BRIAN12682@users.noreply.github.com>
2024-05-09 11:55:22 +0300

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -2,11 +2,11 @@ import random
 import string
 
 def generate_password(length):
-    
-    chars = string.ascii_letters + string.digits
+    chars = string.ascii_letters 
+    # + string.digits
     password = ''.join(random.choice(chars) for i in range(length))
     return password
+    
 length = int(input("Enter password length: "))
-
 password = generate_password(length)
 print("Your password is:", password)
