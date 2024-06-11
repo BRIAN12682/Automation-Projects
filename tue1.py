2a77ae45c48f9a5d584a0b793acf2c6124e003fb
ALABYEKKUBO SSUUBI BRIAN <106748613+BRIAN12682@users.noreply.github.com>
2024-06-11 04:07:51 +0300

    Oop

---
+++ tue1.py
@@ -0,0 +1,30 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} says woof! and I am {self.age} years old ")
+
+my_dog = Dog("Rex", 5)
+my_dog.bark()
+
+#pip install matplotlib
+#import matplotlib.pyplot as plt
+
+#x = [1, 2, 3, 4, 5]
+#y = [2, 3, 5, 7, 11]
+
+#plt.plot(x, y)
+#plt.xlabel('X axis')
+#plt.ylabel('Y axis')
+#plt.title('Simple Plot')
+#plt.show()
+
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+for value in my_generator():
+    print(value)
